025cea3bbf06630daca3ebf25136e24bf44f6f9e
haileyhutch <find@A5-004072.localdomain>
2018-11-28 17:17:37 -0700

    started countdown

---
+++ app.js
@@ -0,0 +1,8 @@
+// countdown to december 25
+
+//the date for today
+//the date we want (december 25)
+//calculation, to figure out the difference in teh time
+//display on HTML side
+
+//eventlistenet, onchange, the change of the difference calculation
